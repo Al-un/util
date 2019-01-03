aaf9fa3c7d47c6ef252e86aa850dd38991acb44a
Al-un <alun.sng@gmail.com>
2019-01-03 10:24:40 +0100

    #1 adding base to vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 // Vue import
 import Vue from 'vue';
 import Router from 'vue-router';
+// Vue config
+import { baseUrl } from '../../vue.config';
 // Components
 import Calculator from 'Components/calculator/Calculator';
 import Home from 'Components/Home';
@@ -11,7 +13,12 @@ Vue.use(Router);
 // Router definition
 export default new Router({
   // path router instead of hash router
+  // https://router.vuejs.org/api/#mode
   mode: 'history',
+  // Routes base
+  // https://router.vuejs.org/api/#mode
+  base: baseUrl,
+  // routes defintions
   routes: [
     { path: '/', name: 'root', component: Home },
     { path: '/calc', name: 'calc', component: Calculator }
